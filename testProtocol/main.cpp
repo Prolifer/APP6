8af21068f29562a5f4b86fe213b701bee9a88f82
Anthony Parris <para2709@usherbrooke.ca>
2018-07-24 11:59:02 -0400

    crc 64 bits works well

--- testProtocol/main.cpp
+++ testProtocol/main.cpp
@@ -12,158 +12,86 @@ unsigned long long rem = 0; //TODO : Change type long long for a type requiring 
 uint8_t dataArr[200] = ""; //TODO : Experiment with real APP situation
 uint8_t dataCRCArr[204] = "";
 
-//int main() //CRC - TRANSMISSION
-//{
-//	// TEST - ARGUMENTS OF THE FUNCTION
-//	uint8_t data = 0b10011101;
-//	uint8_t dataLength = 8;
-//	printf("DATA AS ARGUMENT : %X\n", data);
-//	// TEST - ARGUMENTS OF THE FUNCTION
-//
-//	//Initialize buffer to operate CRC on it
-//	buff = data;
-//	printf("BUFFER OF DATA : %X\n", buff);
-//
-//	//Add N zeroes corresponding to the order of the polynomial
-//	buff = buff << POLY_ORDER;
-//	printf("BUFFER OF DATA WITH ORDER N ZEROS ADDED AT AT LSB : %X\n", buff);
-//
-//	//Register MSB position of the buffer for a first time
-//	int nextMsbPositionBuffer = dataLength + POLY_ORDER;
-//
-//	while ((buff >> (nextMsbPositionBuffer - 1)) == 0 && nextMsbPositionBuffer != 0) { nextMsbPositionBuffer--; }
-//	if (nextMsbPositionBuffer <= 0)
-//		printf("ERROR : msbPositionBuffer is less or equal to zero");
-//	printf("MSB POSITION IN BUFFER AT START : %d\n", nextMsbPositionBuffer);
-//
-//	//Calculate remainder for a first time
-//	rem = (buff >> (nextMsbPositionBuffer - (POLY_ORDER + 1))) ^ POLY_Gx;
-//	printf("CALCULATE FIRST REMAINDER : %X\n", rem);
-//
-//	//Operaiton loop to find final remainder (XOR bitwise with polynomial(Gx) on buffer)
-//	int i;
-//	while (nextMsbPositionBuffer > (POLY_ORDER + 1)){//(nextMsbPositionBuffer >= 0){
-//		//Verify next MSB position in remainder
-//		while (rem != 0 && (rem >> (POLY_ORDER - i)) == 0 && (POLY_ORDER - i) > 0){ i++;}
-//
-//		//Verify number of bits to add remainder to be able to XOR bitwise again
-//		if (rem == 0) {
-//			nextMsbPositionBuffer -= (POLY_ORDER + 1);
-//			while ((((buff << (SIZE_IN_BITS_BUFFER - nextMsbPositionBuffer)) >> (SIZE_IN_BITS_BUFFER - nextMsbPositionBuffer)) >> (nextMsbPositionBuffer - 1)) == 0 && nextMsbPositionBuffer != 0) {nextMsbPositionBuffer--;}
-//			rem = (((buff << (SIZE_IN_BITS_BUFFER - nextMsbPositionBuffer)) >> (SIZE_IN_BITS_BUFFER - nextMsbPositionBuffer)) >> (nextMsbPositionBuffer - (POLY_ORDER + 1)));
-//			printf("\tREM : %X\n", rem);
-//
-//			//Calculate next remainder
-//			rem = rem ^ POLY_Gx;
-//			printf("\tREM : %X\n", rem);
-//		}
-//		else{
-//			if (nextMsbPositionBuffer > (POLY_ORDER + 1)) { //DO SOMETHING WITH THAT
-//				nextMsbPositionBuffer -= (POLY_ORDER + 1 - i);
-//
-//				unsigned long long temp1 = (rem << i);
-//				unsigned long long temp21 = (buff << (SIZE_IN_BITS_BUFFER - nextMsbPositionBuffer));
-//				unsigned long long temp22 = (temp21 >> (SIZE_IN_BITS_BUFFER - nextMsbPositionBuffer));
-//				unsigned long long temp23 = (temp22 >> nextMsbPositionBuffer);//(temp22 >> (nextMsbPositionBuffer - i)); 
-//
-//				unsigned long long temp3 = temp1 + temp23;
-//				//rem = (rem << i) + (((buff << (SIZE_IN_BITS_BUFFER - nextMsbPositionBuffer)) >> (SIZE_IN_BITS_BUFFER - nextMsbPositionBuffer)) >> (nextMsbPositionBuffer - i));
-//				rem = temp3;
-//
-//				printf("\tREM : %X\n", rem);
-//
-//				//Calculate next remainder
-//				rem = rem ^ POLY_Gx;
-//				printf("\tREM : %X\n", rem);
-//			}
-//		}
-//		i = 1;
-//
-//		////Calculate next remainder
-//		//rem = rem ^ POLY_Gx;
-//		//printf("\tREM : %X\n", rem);
-//	}
-//
-//	//rem = rem
-//
-//
-//	//Adding CRC value to the buffer
-//	buff += rem;
-//	
-//	// VERIFYING FUNCTION QUALITY
-//	printf("REMAINDER EXPECTED: %X\n", 0b100);
-//	printf("REMAINDER RESULT: %X\n", rem);
-//	printf("BUFFER+CRC EXPECTED: %X\n", 0b10011101100);
-//	printf("BUFFER+CRC RESULT: %X\n", buff);
-//	if (0b10011101100 == buff && rem == 0b100) 
-//		printf("FUNCTION DONE\n", buff);
-//	else
-//		printf("FUNCTION FAILED\n", buff);
-//
-//	return 0;
-//}
-
 int main() //CRC - TRANSMISSION
 {
 	// TEST - ARGUMENTS OF THE FUNCTION
-	uint8_t data = 0b10011101;
-	uint8_t dataLength = 8;
+	int data = 0b100111010;
+	int dataLength = 9;
 	printf("DATA AS ARGUMENT : %X\n", data);
 	// TEST - ARGUMENTS OF THE FUNCTION
-		   
+
 	//Initialize buffer to operate CRC on it
 	buff = data;
 	printf("BUFFER OF DATA : %X\n", buff);
-		   
+
 	//Add N zeroes corresponding to the order of the polynomial
 	buff = buff << POLY_ORDER;
 	printf("BUFFER OF DATA WITH ORDER N ZEROS ADDED AT AT LSB : %X\n", buff);
-		   
+
 	//Register remaining bits of the buffer for a first time
 	int remBitsToConsiderBuffer = dataLength + POLY_ORDER;
-		   
 	while ((buff >> (remBitsToConsiderBuffer - 1)) == 0 && remBitsToConsiderBuffer != 0) { remBitsToConsiderBuffer--; }
 	if (remBitsToConsiderBuffer <= 0)
 		printf("ERROR : msbPositionBuffer is less or equal to zero, no bits to consider at start");
 	printf("REMAINING BITS TO CONSIDER AT START : %d\n", remBitsToConsiderBuffer);
-	
+
 	//Initialize variable exposing MSB position in remainder
 	int nextMsbPositionRem = (POLY_ORDER + 1); //Remainder always has a MSB position at the 4th digit or less
-
-	//Calculate remainder for first time
-	rem = (buff >> (remBitsToConsiderBuffer - (POLY_ORDER + 1))) ^ POLY_Gx;
-	printf("CALCULATE FIRST REMAINDER : %X\n", rem);
 
 	//Initialize variable indicating needed bits to have enough bits to recalculate a XOR bitwise operation
 	int neededBitsToRem = 0;
 
+	//Calculate remainder for first time
+	rem = (buff >> (remBitsToConsiderBuffer - (POLY_ORDER + 1)));
+	printf("CALCULATE FIRST REMAINDER : %X\n", rem);
+
 	//Operation loop to find final remainder (XOR bitwise with polynomial(Gx) on buffer)
 	while (remBitsToConsiderBuffer > 0){
+		//Calculate next remainder (XOR bitwise)
+		rem = rem ^ POLY_Gx;
+		printf("REM : %X\n", rem);
+
 		//Check where is MSB in remainder
+		nextMsbPositionRem = (POLY_ORDER + 1); //Remainder always has a MSB position at the 4th digit or less
 		while ((rem >> (nextMsbPositionRem - 1)) == 0 && nextMsbPositionRem != 0) { nextMsbPositionRem--; }
 
 		//Check bits needed for remainder
 		neededBitsToRem = (POLY_ORDER + 1) - nextMsbPositionRem;
 
-		//Fill remainder with needed bits for re-operation XOR bitwise
-		rem = rem << neededBitsToRem;
-		rem += (buff << (SIZE_IN_BITS_BUFFER - (remBitsToConsiderBuffer - )))
-				>>
-				(SIZE_IN_BITS_BUFFER - (remBitsToConsiderBuffer - ));
+		if ((remBitsToConsiderBuffer - (POLY_ORDER + 1)) >= 0) {
+			//Calculate remaining bits to consider in the buffer
+			remBitsToConsiderBuffer -= (POLY_ORDER + 1);
+			printf("REMAINING BITS CONSIDER BUFFER : %d\n", remBitsToConsiderBuffer);
 
-		//Calculate next remainder (XOR bitwise)
-		rem = rem ^ POLY_Gx;
-		printf("REM : %X\n", rem);
+			//Fill remainder with needed bits for re-operation XOR bitwise
+			if(neededBitsToRem != 0){ //IMPORTANT****
+			rem = rem << neededBitsToRem;
+			rem += (buff << (SIZE_IN_BITS_BUFFER - remBitsToConsiderBuffer))
+				>>
+				(SIZE_IN_BITS_BUFFER - neededBitsToRem);
+			}
+
+			printf("REM : %X\n", rem);
+		}
+		else {//remBitsToConsiderBuffer < 0
+			
+			neededBitsToRem = remBitsToConsiderBuffer - 1;
+			rem = rem << neededBitsToRem;
+			rem += (buff << (SIZE_IN_BITS_BUFFER - neededBitsToRem))
+				>>
+				(SIZE_IN_BITS_BUFFER - neededBitsToRem);
+			remBitsToConsiderBuffer = 0;
+			printf("REMAINING BITS CONSIDER BUFFER : %d\n", remBitsToConsiderBuffer);
+		}
 	}
 
-		   	
 	// VERIFYING FUNCTION QUALITY
-	printf("REMAINDER EXPECTED: %X\n", 0b100);
+	printf("REMAINDER EXPECTED: %X\n", 0b1000);
 	printf("REMAINDER RESULT: %X\n", rem);
-	if (rem == 0b100) 
+	if (rem == 0b1000)
 		printf("FUNCTION DONE\n", buff);
 	else
 		printf("FUNCTION FAILED\n", buff);
-		   
+
 	return 0;
 }
